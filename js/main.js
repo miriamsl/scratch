aa2894780a1caa4501d06f3d91b78968ec21903c
Miriam Lillebo <miriamlillebo@gmail.com>
2017-10-23 18:42:38 +0200

    Made board

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var table = document.getElementById("board");
+var bokstaver = ["", "A","B","C","D"];
 
 for (i = 0; i <= 10; i++){
     row = table.insertRow(i);
     for(j = 0; j <= 4; j++){
         var cell = row.insertCell(j);
-        cell.innerHTML = "yo";
+        if(i===0){
+        	cell.innerHTML = bokstaver[j];
+        }
+        else if(j===0){
+        	cell.innerHTML = i;
+        } else {
+        	cell.innerHTML = "yo";
+        }
     }
 }
