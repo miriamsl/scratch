bf98708ddd93bb47c99537098355e1c4193002e9
Sondre Wold <sondrewo@hotmail.com>
2017-10-23 18:56:07 +0200

    added random cell

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 var table = document.getElementById("board");
 var bokstaver = ["", "A","B","C","D"];
 
+var brett = new Array(10);
+for (var i = 0; i < 10; i++) {
+  brett[i] = new Array(4);
+}
+
+for (i = 0; i < 10; i++){
+    for(j = 0; j < 4; j++){
+        brett[i][j] = getRandomArbitrary(1,2);
+    }
+}
+
+for(i = 0; i<9; i++){
+
+    brett[i][getRandomArbitrary(0,3)] = 3
+}
+
+console.log(brett)
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 for (i = 0; i <= 10; i++){
     row = table.insertRow(i);
     for(j = 0; j <= 4; j++){
